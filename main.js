33658e26b675ccd764b38158bb1f4c1456df1e13
imt-eiaz <imteiaz@gmail.com>
2025-05-19 16:35:25 +0100

    8:33

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 //Get Request by Axios();
 function getTodos() {
+  //   axios({
+  //     method: "get",
+  //     url: "https://jsonplaceholder.typicode.com/photos",
+  //     params: {
+  //       _limit: 5,
+  //     },
+  //   })
+  //     .then((res) => showOutput(res))
+  //     .catch((err) => console.log(err));
+
   axios({
     method: "get",
-    url: "https://jsonplaceholder.typicode.com/users",
+    url: "https://jsonplaceholder.typicode.com/photos",
+    params: {
+      _limit: 5,
+    },
   })
     .then((res) => showOutput(res))
     .catch((err) => console.log(err));
@@ -13,13 +26,11 @@ function showOutput(res) {
   document.getElementById("res").innerHTML = ` 
   <div class="card mt-3">
     <div class="card-header">
-      Customer Names
+      Data
     </div>
     <div class="card-body">
-      <ul>
-        <li>Name : ${JSON.stringify(res.data[0].name, null, 2)}</li>
-        <li>Name : ${JSON.stringify(res.data[1].name, null, 2)}</li>
-      </ul>
+      <pre>${JSON.stringify(res.data, null, 2)}</pre>
+      
     </div>
   </div>`;
 }
