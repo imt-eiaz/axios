96c6e9127a5fba13089526320775b18b03520d6e
imt-eiaz <imteiaz@gmail.com>
2025-05-19 16:50:06 +0100

    12:30

--- main.js
+++ main.js
@@ -10,30 +10,60 @@ function getTodos() {
   //     .then((res) => showOutput(res))
   //     .catch((err) => console.log(err));
 
-  axios({
-    method: "get",
-    url: "https://jsonplaceholder.typicode.com/photos",
-    params: {
-      _limit: 5,
-    },
-  })
+  axios
+    .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
+}
+
+// Post Request
+function addTodo() {
+  axios
+    .post("https://jsonplaceholder.typicode.com/todos", {
+      title: "New Todo",
+      completed: false,
+    })
     .then((res) => showOutput(res))
     .catch((err) => console.log(err));
 }
 
-//Showoutput in browser
+// Show output in browser
 function showOutput(res) {
-  document.getElementById("res").innerHTML = ` 
+  document.getElementById("res").innerHTML = `
+  <div class="card card-body mb-4">
+    <h5>Status: ${res.status}</h5>
+  </div>
+
+  <div class="card mt-3">
+    <div class="card-header">
+      Headers
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.headers, null, 2)}</pre>
+    </div>
+  </div>
+
   <div class="card mt-3">
     <div class="card-header">
       Data
     </div>
     <div class="card-body">
       <pre>${JSON.stringify(res.data, null, 2)}</pre>
-      
     </div>
-  </div>`;
+  </div>
+
+  <div class="card mt-3">
+    <div class="card-header">
+      Config
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.config, null, 2)}</pre>
+    </div>
+  </div>
+`;
 }
 
 // Event listeners
 document.getElementById("get").addEventListener("click", getTodos);
+
+document.getElementById("post").addEventListener("click", addTodo);
